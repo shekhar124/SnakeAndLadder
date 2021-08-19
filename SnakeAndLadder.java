c073485791cb3654f4852854d8d6f8ab13eb3c20
rajashekhar <rajashekharpasikanti@gmail.com>
2021-08-19 14:34:49 +0530

    Check For An option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,7 +8,21 @@ public class SnakeAndLadder {
 	int diceRoll = 1+(int)Math.floor(Math.random()*10)%6;
 	
 		System.out.println("Dice Rolled:" +diceRoll);
+		int checkOption=(int)Math.floor(Math.random()*10)%3;
+		if (checkOption == 0) {
+			System.out.println("No Play ");
+			
+			System.out.println("the player stays in same position");
+		}
+		else if (checkOption != 0) {
+			System.out.println("Ladder Comes" );
+			position+=diceRoll;
+		}
+		else {
+			System.out.println("Snake Comes");
+			position-= diceRoll;
 		
 	}
 
-}
+	}
+	}

e112a60b28f30376a17ce73d90fe6071d883d4e5
rajashekhar <rajashekharpasikanti@gmail.com>
2021-08-19 13:44:58 +0530

    Wellcome and starting point at 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		int position = 0;
+		System.out.println("well come to snake and ladder position : " + position);
+
+	}
+
+}
